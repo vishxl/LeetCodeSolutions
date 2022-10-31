d5cefb4af24fbffe76b7e3b7000d4426bfe32d72
Vishal Singh <vishalsingh7x@gmail.com>
2022-10-31 13:37:35 +0530

    Solution for count and say

---
+++ python/38-count-and-say.py
@@ -0,0 +1,23 @@
+class Solution:
+    def countAndSay(self, n):
+        seq = "1"
+        for i in xrange(n - 1):
+            seq = self.getNext(seq)
+        return seq
+    
+    def getNext(self, seq):
+        i, next_seq = 0, ""
+        while i < len(seq):
+            cnt = 1        
+            while i < len(seq) - 1 and seq[i] == seq[i + 1]:
+                cnt += 1
+                i += 1
+            next_seq += "{}{}".format(cnt, seq[i])
+            i += 1
+        return next_seq
+
+if __name__ == "__main__":
+    for i in xrange(1, 4):
+        print Solution().countAndSay(i)
+
+        
